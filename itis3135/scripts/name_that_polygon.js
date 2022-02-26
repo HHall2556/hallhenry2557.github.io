bec6cc63cfdb063dea43aa830662437de08e84ba
HHall2556 <89744350+HHall2556@users.noreply.github.com>
2022-02-26 14:22:45 -0500

    name_that_polygon

---
+++ itis3135/scripts/name_that_polygon.js
@@ -0,0 +1,65 @@
+function alertTest()
+{
+    alert('pizza');
+}
+function alertThatPolygon()
+{
+    let numOfSides = document.getElementById("polygongen").value;
+    numOfSides = Math.abs(Math.round(numOfSides));
+    let validation = validateEntry(numOfSides);
+    if(validation)
+    {
+        document.getElementById("polygon_error_message").innerHTML= "Thank you. Have a HOT PINK HIPPO DAY!";
+        switch(numOfSides)
+        {
+            case 1:
+                alert('Henagon');
+                break;
+            case 2:
+                alert('Digon');
+                break;
+            case 3:
+                alert('Trigon');
+                break;
+            case 4:
+                alert('Tetragon');
+                break;
+            case 5:
+                alert('Pentagon');
+                break;
+            case 6:
+                alert('Hexagon');
+                break;
+            case 7:
+                alert('Heptagon');
+                break;
+            case 8:
+                alert('Octagon');
+                break;
+            case 9:
+                alert('Enneagon');
+                break;
+            default:
+                alert('Decagon');
+                break;
+        }
+    }
+    else
+    {
+        document.getElementById("polygon_error_message").innerHTML= "That isn't a valid answer." + 
+        " Try a number between 1 and 10";
+        return false;
+    }
+}
+function validateEntry(entry)
+{
+    if(entry > 0 && entry < 11)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
